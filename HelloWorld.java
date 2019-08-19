4993db52603ad9607724b6dec70608a2fa25c973
selvaraj-alagarsamy <52665459+selvaraj-alagarsamy@users.noreply.github.com>
2019-08-19 20:12:52 +0530

    This is First Commit. Created HelloWorld Program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args){
+    System.out.println("This is Hello World Program...");
+    System.out.println("Welcome to GitHub");
+    System.out.println("Ofcourse. This is my first project in Github");
+  }
+}
